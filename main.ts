29dd9068849c08e7640bcc1f858cd6cc508589a7
lumex57 <133763368+lumex57@users.noreply.github.com>
2025-09-30 15:50:50 +0900

    asdfgggg

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
 basic.forever(function () {
-	
+    if (pins.analogReadPin(AnalogReadWritePin.P0) < 10) {
+        music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    }
 })
