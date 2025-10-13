e113d7819d57faf08f0c44d42f941099cc19a980
lumex57 <133763368+lumex57@users.noreply.github.com>
2025-10-13 11:30:36 +0900

    때려치고싶ㅍ다

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let geori: number
+
+basic.forever(function () {
+    // if (geori)
+    console.log(geori)
+    // sonar.ping(DigitalPin.P2, DigitalPin.P2, PingUnit.MicroSeconds)
+})
 basic.forever(function () {
     if (pins.analogReadPin(AnalogReadWritePin.P1) < 10) {
         music.play(music.stringPlayable("C5 - - - - - - - ", 120), music.PlaybackMode.UntilDone)
