a74770757f8ce65ff523c0eead2563e01b56e99e
Seojin Lee <133763368+lumex57@users.noreply.github.com>
2025-10-20 11:10:07 +0900

    Done. EZ XDD

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
-let geori: number
-// sonar.ping(DigitalPin.P2, DigitalPin.P2, PingUnit.MicroSeconds)
-basic.forever(function () {
-    console.log(geori)
-})
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
 basic.forever(function () {
     if (pins.analogReadPin(AnalogReadWritePin.P1) < 10) {
         music.play(music.stringPlayable("C5 - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    }
+})
+basic.forever(function () {
+    huskylens.request()
+    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            music.play(music.stringPlayable("C5 - C5 - C5 - C5 - ", 300), music.PlaybackMode.UntilDone)
+        } else {
+        	
+        }
     }
 })
